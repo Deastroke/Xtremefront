2e5ab127a65afec77e26f3e49f08e06153c1a047
Deastroke <163570672+Deastroke@users.noreply.github.com>
2025-07-29 18:32:28 -0600

    Update form.js

--- Xtreme/js/form.js
+++ Xtreme/js/form.js
@@ -1,30 +1,27 @@
+const BACKEND_URL = "https://xtremebackend.onrender.com";
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault(); // Evita el envío inmediato
 
-        // Crear un objeto FormData para enviar los datos del formulario
         const formData = new FormData(form);
-
-        // Crear un objeto URLSearchParams para serializar los datos en formato adecuado
         const urlEncodedData = new URLSearchParams();
         for (const pair of formData.entries()) {
             urlEncodedData.append(pair[0], pair[1]);
         }
 
-        // Enviar el formulario de manera asincrónica
-        fetch('/submit', {
+        fetch(`${BACKEND_URL}/submit`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: urlEncodedData
         })
-        .then(response => response.text())  // Espera la respuesta en texto (mensaje de éxito o error)
+        .then(response => response.text())
         .then(data => {
-            // Muestra el mensaje de éxito si el servidor responde
-            mostrarAlerta(data, "success");  // La respuesta será "Mensaje enviado con éxito"
+            mostrarAlerta(data, "success");
         })
         .catch(error => {
             console.error('Error al enviar el mensaje:', error);
@@ -33,14 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
 function mostrarAlerta(mensaje, tipo) {
     const alerta = document.getElementById("alerta");
     const alertaMensaje = document.getElementById("alerta-mensaje");
 
-    // Si el tipo es "success", agrega la palomita
     if (tipo === "success") {
-        alertaMensaje.innerHTML = "✔️ " + mensaje;  // Esto incluirá la palomita en el mensaje
+        alertaMensaje.innerHTML = "✔️ " + mensaje;
     } else {
         alertaMensaje.textContent = mensaje;
     }
@@ -48,8 +43,8 @@ function mostrarAlerta(mensaje, tipo) {
     alerta.className = "alerta " + tipo;
     alerta.style.display = "block";
 
-    // Ocultar después de 3 segundos
     setTimeout(() => {
         alerta.style.display = "none";
     }, 4000);
 }
+
